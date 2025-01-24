60bde4510e40f39a7bf2709b244992dead0328ee
Guaaannnn <mfatkhanptr@gmail.com>
2025-01-24 21:37:26 +0700

    Tes UI

---
+++ Assets/Scenes/Perpindahan.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Perpindahan : MonoBehaviour
+{ 
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("");
+    }
+
+    void Update()
+    {
+        
+    }
+}
