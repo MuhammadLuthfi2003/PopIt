b80929d1ad77a7affabc307b16e54c0a0becea3c
MuhammadLuthfi2003 <88194674+MuhammadLuthfi2003@users.noreply.github.com>
2025-01-25 10:44:18 +0700

    add UI functionality

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject controlsMenu;
+    public GameObject creditsMenu;
+    public GameObject playMenu;
+    public GameObject settingsMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controlsMenu.SetActive(false);
+        creditsMenu.SetActive(false);
+        playMenu.SetActive(false);
+    }
+    
+    public void PlayButtonPressed()
+    {
+        mainMenu.SetActive(false);
+        playMenu.SetActive(true);
+    }
+
+    public void SettingsButtonPressed()
+    {
+        mainMenu.SetActive(false);
+        controlsMenu.SetActive(true);
+    }
+
+    public void CreditsButtonPressed()
+    {
+        mainMenu.SetActive(false);
+        creditsMenu.SetActive(true);
+    }
+
+    public void BackToMainMenu()
+    {
+        mainMenu.SetActive(true);
+        controlsMenu.SetActive(false);
+        creditsMenu.SetActive(false);
+        playMenu.SetActive(false);
+    }
+}
