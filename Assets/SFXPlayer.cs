878ec425e40ff6c8cd524bd2cd4e2517775e2903
MuhammadLuthfi2003 <88194674+MuhammadLuthfi2003@users.noreply.github.com>
2025-01-25 20:10:28 +0700

    audio dump

---
+++ Assets/SFXPlayer.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXPlayer : MonoBehaviour
+{
+    private static SFXPlayer instance;
+
+    private AudioSource audioSource;
+
+    [Header("Gameplay SFX")]
+    public AudioClip explosionSFX;
+    public AudioClip bubblePopSFX;
+    public AudioClip bubbleSpawnSFX;
+    public AudioClip speedSFX;
+    public AudioClip stunSFX;
+
+    [Header("UI SFX")]
+    public AudioClip buttonClickSFX;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
+    public void PlayExplosionSFX()
+    {
+        audioSource.PlayOneShot(explosionSFX);
+    }
+
+    public void PlayBubblePopSFX()
+    {
+        audioSource.PlayOneShot(bubblePopSFX);
+    }
+
+    public void PlayBubbleSpawnSFX()
+    {
+        audioSource.PlayOneShot(bubbleSpawnSFX);
+    }
+
+    public void PlaySpeedSFX()
+    {
+        audioSource.PlayOneShot(speedSFX);
+    }
+
+    public void PlayStunSFX()
+    {
+        audioSource.PlayOneShot(stunSFX);
+    }
+
+    public void PlayButtonClickSFX()
+    {
+        audioSource.PlayOneShot(buttonClickSFX);
+    }
+}
